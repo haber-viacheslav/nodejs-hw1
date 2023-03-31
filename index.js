4ce7a555b4a3e20138d513eb5eaffd2d86055efb
Viacheslav Haber <gaberslavik@gmail.com>
2023-03-31 15:32:49 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const { contacts, listContacts, addContact, getContactById, removeContact } = require("./contacts");
+
+const cont = contacts.then((res) => console.log(res));
+console.log(cont);
+//drfsvdaesdeswvf
+
+// cont.push({
+// 	id: "rsKkOQUi80UsgVPCcLZZW34dfds",
+// 	name: "Alex Mordac",
+// 	email: "elementum@sceled.net",
+// 	phone: "(748) 206-3588",
+// });
---
+++ user.js
@@ -0,0 +1,12 @@
+const user = {
+	name: "Alex",
+	age: 20,
+	position: "developer",
+};
+
+module.exports = {
+	user,
+	sayHello(msg) {
+		console.log(msg);
+	},
+};
